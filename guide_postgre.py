464074a53b14b7b253751c1dcba13f720144b92b
Simon Gerula <saimonsir@gmail.com>
2020-09-17 17:48:34 -0300

    fix

--- guide_postgre.py
+++
@@ -1,6 +0,0 @@
-Instalar pip install psycopg2, para poder conectar la BBDD PostgreSQL
-
-import psycopg2
-
-Conectar PostgreeSQL > 
-    psycopg2.connect(dbname="postgres", user="postgres", password="admin", host="localhost", port="5432")
--- prueba.py
+++
@@ -1,9 +0,0 @@
-ctn = ()
-ctn_list = list(ctn)
-for i in range(2):
-    var = input(": ")
-    ctn_list.append(var)
-ctn = tuple(ctn_list)
-print(ctn)
-
-print("hola %s") %(ctn_list)
